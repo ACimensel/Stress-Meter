1649690322ab577abcabd6c443fdb2cc950af824
Artun Cimensel <49254881+ACimensel@users.noreply.github.com>
2022-10-19 22:12:59 -0700

    table is filled out with csv data

--- app/src/main/java/com/example/stressmeter/fragments/ResultsFragment.kt
+++ app/src/main/java/com/example/stressmeter/fragments/ResultsFragment.kt
@@ -1,15 +1,14 @@
 package com.example.stressmeter.fragments
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TableLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.Fragment
 import com.example.stressmeter.R
-import lecho.lib.hellocharts.model.AxisValue
-import lecho.lib.hellocharts.model.PointValue
 import java.io.*
 
 
@@ -18,29 +17,41 @@ class ResultsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_results, container, false)
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
 
         val file = File(requireActivity().getExternalFilesDir(null), "StressLevels.csv")
         if (!file.exists()) {
             file.createNewFile()
         }
         else{
-            val buffReader = file.bufferedReader()
-
-            buffReader.forEachLine {
-                println("value = $it")
-            }
-
-
+            fillTable(file)
         }
 
 
     }
 
+    private fun fillTable(file: File){
+        file.bufferedReader().forEachLine {
+            val stressLevels = it.split(",").toTypedArray()
 
+            val row = TableRow(requireActivity())
+            row.layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT)
+
+            val tv0 = TextView(requireActivity())
+            tv0.layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 1f)
+            tv0.text = stressLevels[0]
+            tv0.setBackgroundColor(Color.LTGRAY)
+            row.addView(tv0)
+
+            val tv1 = TextView(requireActivity())
+            tv1.layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 1f)
+            tv1.text = stressLevels[1]
+            row.addView(tv1)
+
+            val table = requireActivity().findViewById<TableLayout>(R.id.table_csv_results)
+            table.addView(row)
+        }
+    }
 
 }
